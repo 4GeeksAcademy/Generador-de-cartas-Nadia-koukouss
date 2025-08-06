948653c4600471a2352553d4252b1f283e1118b0
Nadia-2025 <nadyah-88@hotmail.com>
2025-08-06 10:59:54 +0200

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,62 @@
-import "bootstrap";
-import "./style.css";
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+const palos = ["corazones", "picas", "treboles", "diamantes"];
 
+const symbols = {
+  corazones: "♥",
+  picas: "♠",
+  treboles: "♣",
+  diamantes: "♦",
+};
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let intervalId = null;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  const card = document.querySelector(".card");
+  const button = document.querySelector(".button-js");
+  const startBtn = document.querySelector(".startBtn-js");
+  const stopBtn = document.querySelector(".stopBtn-js");
+
+  function generateCard() {
+    card.innerHTML = "";
+
+    const cardNumber = Math.floor(Math.random() * numbers.length);
+    const cardPalo = Math.floor(Math.random() * palos.length);
+
+    const palo = palos[cardPalo];
+    const number = numbers[cardNumber];
+    const color =
+      palo === "corazones" || palo === "diamantes" ? "red" : "black";
+
+    const div1 = document.createElement("div");
+    div1.classList.add("div1");
+    div1.innerHTML = `<span style="color: ${color}">${number} ${symbols[palo]}</span>`;
+
+    const div2 = document.createElement("div");
+    div2.classList.add("div2");
+    div2.innerHTML = `<span style="color: ${color}">${symbols[palo]}</span>`;
+
+    const div3 = document.createElement("div");
+    div3.classList.add("div3");
+    div3.innerHTML = `<span style="color: ${color}">${number} ${symbols[palo]}</span>`;
+
+    card.appendChild(div1);
+    card.appendChild(div2);
+    card.appendChild(div3);
+  }
+  generateCard();
+
+  button.addEventListener("click", generateCard);
+
+  startBtn.addEventListener("click", () => {
+    if (!intervalId) {
+      intervalId = setInterval(() => {
+        generateCard();
+      }, 3000); //aqui he puesto 3s en lugar de los 10s del ejercicio, porque es más rápido para ir probando código ;)//
+    }
+  });
+
+  stopBtn.addEventListener("click", () => {
+    clearInterval(intervalId);
+    intervalId = null;
+  });
 };
