ecae22d58a6e024fd1106d9856edd28160e7d8fe
Nadia-2025 <nadyah-88@hotmail.com>
2025-08-06 11:27:46 +0200

    rectify functionality

--- src/app.js
+++ src/app.js
@@ -24,20 +24,27 @@ window.onload = function () {
 
     const palo = palos[cardPalo];
     const number = numbers[cardNumber];
-    const color =
-      palo === "corazones" || palo === "diamantes" ? "red" : "black";
+
+    const paloClass = {
+      corazones: "heart",
+      diamantes: "diamond",
+      picas: "spade",
+      treboles: "club",
+    };
+
+    card.className = `card ${paloClass[palo]}`;
 
     const div1 = document.createElement("div");
     div1.classList.add("div1");
-    div1.innerHTML = `<span style="color: ${color}">${number} ${symbols[palo]}</span>`;
+    div1.innerHTML = `<span>${number} ${symbols[palo]}</span>`;
 
     const div2 = document.createElement("div");
     div2.classList.add("div2");
-    div2.innerHTML = `<span style="color: ${color}">${symbols[palo]}</span>`;
+    div2.innerHTML = `<span>${symbols[palo]}</span>`;
 
     const div3 = document.createElement("div");
     div3.classList.add("div3");
-    div3.innerHTML = `<span style="color: ${color}">${number} ${symbols[palo]}</span>`;
+    div3.innerHTML = `<span>${number} ${symbols[palo]}</span>`;
 
     card.appendChild(div1);
     card.appendChild(div2);
